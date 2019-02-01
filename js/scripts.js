5c843cc5c1a11f5f3c40d80262eeb1ecf54970d3
Omar Kadah <omarmkadah@gmail.com>
2019-02-01 14:46:19 -0800

    functions added, user input registering

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,33 @@ function Pizza(size, crust, sauce, topping) {
   this.sauce = sauce;
   this.topping = topping;
 }
+
+// Pizza.prototype.calcPrice = (function) {
+//   var cost = this.movie.price;
+// }
+
+function pizzaToppings () {
+  var toppings = [];
+  $("#topping-select input:checkbox[name='topping']:checked").each(function(topping){
+    toppings.push($(this).val());
+  });
+}
+
+
+
+
+
+
+$(document).ready(function() {
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
+    var inputSize = $("#size").val();
+    var inputCrust = $("#crust").val();
+    var inputSauce = $("#sauce").text();
+    // console.log(inputMovie + inputPrice);
+    $("topping-select").val();
+    var pizzaPrice = new Pizza (inputSize, inputCrust, inputSauce);
+    console.log(pizzaPrice);
+    // $(".ticketprice").text(ticketPrice.calcPrice());
+  })
+})
