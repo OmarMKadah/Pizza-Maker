74f3af5de5a489e5ae418806ce35c4c361b16425
Omar Kadah <omarmkadah@gmail.com>
2019-02-06 23:12:52 -0800

    restructure code, calculated price based on size

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-function Pizza(size, crust, sauce, topping) {
+function Pizza(size, crust, sauce) {
   this.size = size;
   this.crust = crust;
   this.sauce = sauce;
-  this.toppings = topping;
+  this.price= 0;
 }
 
-// Pizza.prototype.calcSizePrice = (function) {
-//   if (this.size === "8") {
-//     this.price += 2;
-//   } else if (this.size === "12") {
-//     this.price += 4;
-//   } else if (this.size === "16") {
-//     this.price += 6;
-//   }
-// }
-//
+Pizza.prototype.calcSizePrice = function() {
+  if (this.size === "8") {
+    this.price += 2;
+  } else if (this.size === "12") {
+    this.price += 4;
+  } else if (this.size === "16") {
+    this.price += 6;
+  }
+  console.log(this.price);
+}
 // Pizza.prototype.calcToppingPrice = (function) {
 //   for (var i = 0; i < this.toppings.length; i++) {
 //   if (this.cheese[i]) {
@@ -22,15 +22,15 @@ function Pizza(size, crust, sauce, topping) {
 //     }
 //   }
 // }
-
-function pizzaToppings () {
-  var toppings = [];
-  $("#topping-select input:checkbox[name='topping']:checked").each(function(topping){
-    toppings.push($(this).val());
-  });
-  return toppings;
-}
-
+//
+// function pizzaToppings () {
+//   var toppings = [];
+//   $("#topping-select input:checkbox[name='topping']:checked").each(function(topping){
+//     toppings.push($(this).val());
+//   });
+//   return toppings;
+// }
+//
 
 
 
@@ -42,9 +42,9 @@ $(document).ready(function() {
     var inputSize = $("#size").val();
     var inputCrust = $("#crust").val();
     var inputSauce = $("#sauce").val();
-    var inputTopping = pizzaToppings();
-    var pizzaPrice = new Pizza (inputSize, inputCrust, inputSauce);
-    console.log(pizzaPrice);
-    // $(".ticketprice").text(pizzatPrice.calcPrice());
+    var pizza = new Pizza (inputSize, inputCrust, inputSauce);
+    $("#size").val("");
+    $(".pizzacost").text(pizza.calcSizePrice(""));
+    // console.log(calcSizePrice);
   })
 })
